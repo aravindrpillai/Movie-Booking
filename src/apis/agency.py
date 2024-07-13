22df78a7d461f049524fb9caa5aaef5d9d41ff61
Amal Krishnan <amalkrishnan1471@gmail.com>
2024-07-13 21:05:04 +0530

    updated agency to handle data updation

--- src/apis/agency.py
+++ src/apis/agency.py
@@ -7,21 +7,30 @@ def add_agency(request):
     data = request.data
     
     try:   
+        agency_id = data.get('agency_id', None)
+        agency = Agency.objects.filter(agency_id=agency_id)
+        if(agency_id!= None and (not agency.exists())):
+            raise Exception('Invalid agency_id')
+        
         name = data.get('name')
         if(name == '' or name == None):
             raise Exception('agency name cannot be empty')
-            
-        agency_id = Agency.objects.get_next_agency_id()
-        obj = Agency()
+        
+        if(agency.exists()):
+            obj = agency.first()
+        else:
+            obj = Agency()
+            obj.agency_id = Agency.objects.get_next_agency_id()
         obj.name = name
         obj.website = data.get('website')
         obj.email = data.get('email')
-        obj.agency_id = agency_id # 'M'+str(random.randint(100,999))
         obj.save()
 
         dict = {
             'agency_id' : obj.agency_id,
-            'name' : obj.name
+            'name' : obj.name,
+            'website' : obj.website,
+            'email' : obj.email
         }
         return HttpUtil.respond(201, None, dict)
     except Exception as e0: 
